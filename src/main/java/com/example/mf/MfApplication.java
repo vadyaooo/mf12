f15ba017ac485cddebe5bd826d91ff9ee44b6311
Vadim Kuzhko <leonidovich1998@gmail.com>
2022-07-29 14:00:41 +0300

    f

---
+++ src/main/java/com/example/mf/MfApplication.java
@@ -0,0 +1,13 @@
+package com.example.mf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MfApplication.class, args);
+    }
+
+}
