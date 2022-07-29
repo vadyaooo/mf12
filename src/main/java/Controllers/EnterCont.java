922bf969ca6c6b3f5a6566796c4c1e4e57e15e64
Vadim Kuzhko <leonidovich1998@gmail.com>
2022-07-29 14:33:04 +0300

    3

--- src/main/java/Controllers/EnterCont.java
+++ src/main/java/Controllers/EnterCont.java
@@ -1,13 +1,11 @@
 package Controllers;
 
 
-import Entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import repo.UserRepo;
 
 @RestController
 @RequestMapping
